99e7acaf21a2815f16891550295684aa3e9e2c2a
karim <karimashraf890@outlook.com>
2026-03-20 17:01:18 +0200

    kkk

--- components/input/index.tsx
+++ components/input/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from 'react';
+
 type CustomInputProps = {
   type: string;
   className?: string;
@@ -13,15 +15,31 @@ export function CustomInput({
   id,
   placeholder,
 }: CustomInputProps) {
+  // const inputRef = useRef(null);
+  // const [val, setVal] = useState(9);
+
+  // function getUsers(v: any) {}
+
+  // useEffect(() => {
+  //   getUsers(val);
+  // }, [val]);
+
   return (
     <div className='form-group'>
       <label htmlFor={id}>{labeltext}</label>
       <br />
+      {/* {val}
+      {val ? 'ah' : 'kok'}
+      {val && 'karim'}
+      {[1, 3, 4].map((value, index) => {
+        return <div>{value}</div>;
+      })} */}
       <input
         className={className}
         type={type}
         id={id}
         placeholder={placeholder}
+        // onClick={() => setVal(20)}
       />
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,33 @@
-import "./home.scss";
-import "./common/sideMenu.scss";
-import { Link } from "../components/link";
-import { useState } from "react";
-import ProfileLink from "../components/profileLink";
-import SideMenu from "../components/sideMenu";
-import NavBar from "../components/navBar";
+import './home.scss';
+import './common/sideMenu.scss';
+import { Link } from '../components/link';
+import { useEffect, useState } from 'react';
+import ProfileLink from '../components/profileLink';
+import SideMenu from '../components/sideMenu';
+import NavBar from '../components/navBar';
+import { refreshTokenFun } from '../api/api';
+import { isEmail, removeError, showError } from '../helpers/helper';
 
 export function HomePage() {
+  useEffect(() => {
+    let token = localStorage.getItem('accessToken');
+    if (!token) {
+      window.location.href = '/login';
+    }
+
+    let refreshToken = localStorage.getItem('refreshToken');
+
+    const apiUrl = 'http://localhost:9696';
+  }, []);
   return (
-    <div className="container-fluid">
-      <div className="row">
+    <div className='container-fluid'>
+      <div className='row'>
         <SideMenu />
-        <div className="col-10" id="home">
+        <div className='col-10' id='home'>
           <NavBar />
-          <div className="row welcome-box">
-            <div className="col-12 text-center">
-              <h1 className="welcome-title">Welcome</h1>
+          <div className='row welcome-box'>
+            <div className='col-12 text-center'>
+              <h1 className='welcome-title'>Welcome</h1>
             </div>
           </div>
         </div>
